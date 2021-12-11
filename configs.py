90ff63db7a16e22605239200cfdfacd2737f5973
TTXSA <95358866+TTXSA@users.noreply.github.com>
2021-12-11 18:27:03 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,8 +17,8 @@ class Configs:
     BOT_TOKEN = getenv("BOT_TOKEN", "123:abc")
     OWNER_ID = int(getenv("OWNER_ID", "0123"))
     SESSION = getenv("SESSION", "session")
-    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/solidprojects")
-    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/solidprojects_chat")
+    CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/FARZBotsAz")
+    GROUP_LINK = getenv("GROUP_LINK", "https://t.me/FARZSupport")
     UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://t.me/FarzMusicAsistan")
     AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1800"))
 
