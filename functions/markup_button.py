58ec7ea26c1cfad56c1339283f4d252ff9209ff3
Spotifyxy <96356974+Spotifyxy@users.noreply.github.com>
2021-12-20 16:52:29 +0300

    Update markup_button.py

--- functions/markup_button.py
+++ functions/markup_button.py
@@ -54,7 +54,7 @@ def start_markup(chat_id: int, bot_username: str):
             [
                 InlineKeyboardButton(
                     gm(chat_id, "source_code"),
-                    url="https://t.me/FarzMusicAsistan",
+                    url="https://t.me/spofitypromusicasistan",
                 )
             ],
         ]
